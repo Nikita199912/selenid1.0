118856744ea4c6097ea14b17dd610cf2295124ba
nikita <nikitka.shapovalov.99.99@mail.ru>
2025-11-25 21:24:54 +0300

    обавлен тест CardDeliveryTest и обновлён build.gradle

--- src/test/java/ru/netology/selenide/DeliveryCardTest.java
+++ src/test/java/ru/netology/selenide/DeliveryCardTest.java
@@ -26,20 +26,14 @@ public class DeliveryCardTest {
     @Test
     void shouldSuccessfulOrderWithBelgorodAndEvgeniy() {
         $("[data-test-id='city'] input").setValue("Белгород");
-
         String planningDate = generateDate(4, "dd.MM.yyyy");
         $("[data-test-id='date'] input")
                 .sendKeys(Keys.chord(Keys.CONTROL, "a"), Keys.BACK_SPACE);
         $("[data-test-id='date'] input").setValue(planningDate);
-
         $("[data-test-id='name'] input").setValue("Евгений Степанович");
-
         $("[data-test-id='phone'] input").setValue("+89092064650");
-
         $("[data-test-id='agreement'] .checkbox__box").click();
-
         $$(".button").find(Condition.visible).click();
-
         $("[data-test-id='notification']")
                 .shouldBe(Condition.visible, Duration.ofSeconds(15))
                 .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate));
